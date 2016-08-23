12af4be1b40de73493bf51a84b0e5ae9ebd998f3
iantoon0 <ihunickesmith@gmail.com>
2016-08-23 13:45:34 -0500

    Added hero classes

--- src/encounter/Hero.java
+++ src/encounter/Hero.java
@@ -1,95 +1,78 @@
 package encounter;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Hero extends EncounterActor {
-	public int level, xp, proficiencyBonus, acrobatics, animalHandling, 
-	arcana, athletics, deception, history, insight, intimidation, 
-	investigation, medicine, nature, perception, performance, 
-	persuasion, religion, sleightOfHand, stealth, survival, nextLvlXP, gold;
+	public int level, xp, proficiencyBonus, nextLvlXP, gold, hpGainedPerLevel;
 	public boolean inspiration, spellcaster;
 	String name, race;
 	ArrayList<Spell> spellsKnown;
 	ArrayList<Spell> spellsPrepared;
-	ArrayList<String> proficiencies, languages, feats;
-	ArrayList<Integer> trainedSkills;
+	ArrayList<String> proficiencies, languages, feats, skillProficiencies;
+	HashMap<String, Integer> skillMap;
 	ArrayList<Item> inventory;
-	final int[] XP_LEVELS = {0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000};
-	
+		
 	public Hero(){
 		nextLvlXP = 300;
 		level = 1;
 		xp = 0;
 		proficiencyBonus = 2;
-		
+		calculateSkillBon();
+		switch (race){
+		case "hillDwarf": con += 2; wis++; hp++; hpGainedPerLevel++;
+			break;
+		case "mtnDwarf": con += 2; str += 2; proficiencies.add("light armor"); proficiencies.add("medium armor");
+			break;
+		case "highElf": dex += 2; intel++; 
+			break;
+		case "darkElf": dex += 2; cha++;
+			break;
+		case "woodElf": dex += 2; wis++;
+			break;
+		}
 	}
 	
 	public void levelUp(){
 		level++;
-		nextLvlXP = XP_LEVELS[level];
+		nextLvlXP = Constants.XP_LEVELS[level];
 		if ((level - 1) % 4 == 0){
 			proficiencyBonus++;
+		}
+		if(level % 4 == 0){
+			
 		}
 		calculateSkillBon();
 	}
 	
-	public void calculateSkillBon(){
-		acrobatics = dexBon;
-		animalHandling = wisBon;
-		arcana = intelBon;
-		athletics = strBon;
-		deception = chaBon;
-		history = intelBon;
-		insight = wisBon;
-		intimidation = chaBon;
-		investigation = intelBon;
-		medicine = wisBon;
-		nature = intelBon;
-		perception = wisBon;
-		performance = chaBon;
-		persuasion = chaBon;
-		religion = intelBon;
-		sleightOfHand = dexBon;
-		stealth = dexBon;
-		survival = dexBon;
-		for(int i = 0; i < trainedSkills.size(); i++){
-			switch (trainedSkills.get(i)){
-				case 1:  acrobatics += proficiencyBonus;
-            		break;
-				case 2:  animalHandling += proficiencyBonus;
-					break;
-				case 3:  arcana += proficiencyBonus;
-					break;
-				case 4:  athletics += proficiencyBonus;
-					break;
-				case 5:  deception += proficiencyBonus;
-					break;
-				case 6:  history += proficiencyBonus;
-            		break;
-				case 7:  insight += proficiencyBonus;
-            		break;
-				case 8:  intimidation += proficiencyBonus;
-            		break;
-				case 9:  medicine += proficiencyBonus;
-            		break;
-				case 10: nature += proficiencyBonus;
-            		break;
-				case 11: perception += proficiencyBonus;
-					break;
-				case 12: performance += proficiencyBonus;
-					break;
-				case 13: persuasion += proficiencyBonus;
-					break;
-				case 14: religion += proficiencyBonus;
-					break;
-				case 15: sleightOfHand += proficiencyBonus;
-					break;
-				case 16: stealth += proficiencyBonus;
-					break;
-				case 17: survival += proficiencyBonus;
-					break;
-				default: break;
-			}
+	public void addXP(int xp){
+		this.xp += xp;
+		if (this.xp >= nextLvlXP){
+			levelUp();
 		}
+	}
+	
+	public void calculateSkillBon(){
+		skillMap.put("acrobatics", dexBon);
+		skillMap.put("animalHandling", wisBon);
+		skillMap.put("arcana", intelBon);
+		skillMap.put("athletics", strBon);
+		skillMap.put("deception", chaBon);
+		skillMap.put("history", intelBon);
+		skillMap.put("insight", wisBon);
+		skillMap.put("intimidation", chaBon);
+		skillMap.put("investigation", intelBon);
+		skillMap.put("medicine", wisBon);
+		skillMap.put("nature", intelBon);
+		skillMap.put("perception", wisBon);
+		skillMap.put("performance", chaBon);
+		skillMap.put("persuasion", chaBon);
+		skillMap.put("religion", intelBon);
+		skillMap.put("sleightOfHand", dexBon);
+		skillMap.put("stealth", dexBon);
+		skillMap.put("survival", dexBon);
+		for (String s : skillProficiencies){
+			skillMap.put(s, skillMap.get(s) + proficiencyBonus);
+		}
+		
 	}
 }
