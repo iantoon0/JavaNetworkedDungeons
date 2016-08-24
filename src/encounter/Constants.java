bb797a11b0d2f3eb5c46ff11d24f3849bdc78fd6
iantoon0 <ihunickesmith@gmail.com>
2016-08-23 19:10:36 -0500

    Added main method

--- src/encounter/Constants.java
+++ src/encounter/Constants.java
@@ -4,5 +4,4 @@ import java.util.*;
 
 public final class Constants {
 	final static int[] XP_LEVELS = {0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000};
-	final static HashMap<String, Feature> FEATURE_MAP = new HashMap<>();
 }
---
+++ src/encounter/JavaNetworkedDungeons.java
@@ -0,0 +1,13 @@
+package encounter;
+
+import java.util.*;
+
+public class JavaNetworkedDungeons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, Feature> FEATURE_MAP = new HashMap<>();
+		FEATURE_MAP.put();
+	}
+
+}
