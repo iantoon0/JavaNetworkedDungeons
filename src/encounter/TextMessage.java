90ec4f47ecc1541a5c0f3251b5c34deabb9af711
iantoon0 <ihunickesmith@gmail.com>
2016-10-17 19:52:45 -0500

    Added text messages

---
+++ src/encounter/TextMessage.java
@@ -0,0 +1,10 @@
+package encounter;
+
+public class TextMessage {
+	public String strLanguage;
+	public String strContents;
+	public TextMessage(String lang, String contents){
+		this.strContents = contents;
+		this.strLanguage = lang;
+	}
+}
