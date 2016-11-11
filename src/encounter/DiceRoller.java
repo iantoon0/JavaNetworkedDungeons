6638beb0a245468d977c5be1c4d0d0b518add08e
mightypine <morganelizabethbonner@gmail.com>
2016-11-11 12:51:34 -0600

    Made rolling dice more efficient
    Replaced methods for specific cases with one method called dice in
    DiceRoller that works for any size of die. Also updated Hero- removed
    cases and replaced it to work with the updated version of DiceRoller.

--- src/encounter/DiceRoller.java
+++ src/encounter/DiceRoller.java
@@ -4,8 +4,6 @@ import java.math.*;
 
 public class DiceRoller 
 {
-	/* Why not do this? You'd only need the one method
-
  	public int dice(int dNumber, int numberRolled, String playerName, boolean isHidden)
 	{
 		int result = 0;
@@ -24,106 +22,4 @@ public class DiceRoller
 		return result;
 	}
 	
-			Thennnn in Hero you could put
-				iHP += (diceNumber * iConMod) + dr.dice(diceType, diceNumber, sName, false);
-			around line 830 and not have to use any cases
-	*/
-	
-	public int d20(int numberRolled, String playerName, boolean isHidden)
-	{
-		int result = 0;
-		for (int i = 0; i < numberRolled; i++){
-			result += (int) Math.ceil(20 * Math.random());
-		}
-		
-		if(numberRolled == 1){
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a " + result + " on a d20!");
-		}
-		else{
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a total of " + result + " out of " + numberRolled + " d20!");
-		}
-		
-		return result;
-	}
-	public int d4(int numberRolled, String playerName, boolean isHidden)
-	{
-		int result = 0;
-		for (int i = 0; i < numberRolled; i++){
-			result += (int) Math.ceil(4 * Math.random());
-		}
-		
-		if(numberRolled == 1){
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a " + result + " on a d4!");
-		}
-		else{
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a total of " + result + " out of " + numberRolled + " d4!");
-		}
-		return result;
-	}
-	public int d6(int numberRolled, String playerName, boolean isHidden){
-		int result = 0;
-		for (int i = 0; i < numberRolled; i++){
-			result += (int) Math.ceil(6 * Math.random());
-		}
-		if(numberRolled == 1){
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a " + result + " on a d6!");
-		}
-		else{
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a total of " + result + " out of " + numberRolled + " d6!");
-		}
-		return result;
-	}
-	public int d8(int numberRolled, String playerName, boolean isHidden){
-		int result = 0;
-		for (int i = 0; i < numberRolled; i++){
-			result += (int) Math.ceil(8 * Math.random());
-		}
-		if(numberRolled == 1){
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a " + result + " on a d8!");
-		}
-		else{
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a total of " + result + " out of " + numberRolled + " d8!");
-		}
-		return result;
-	}
-	public int d10(int numberRolled, String playerName, boolean isHidden){
-		int result = 0;
-		for (int i = 0; i < numberRolled; i++){
-			result += (int) Math.ceil(10 * Math.random());
-		}
-		if(numberRolled == 1){
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a " + result + " on a d10!");
-		}
-		else{
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a total of " + result + " out of " + numberRolled + " d10!");
-		}
-		return result;
-	}
-	public int d12(int numberRolled, String playerName, boolean isHidden){
-		int result = 0;
-		for (int i = 0; i < numberRolled; i++){
-			result += (int) Math.ceil(12 * Math.random());
-		}
-		if(numberRolled == 1){
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a " + result + " on a d12!");
-		}
-		else{
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a total of " + result + " out of " + numberRolled + " d12!");
-		}
-		return result;
-	}
-	public int d100(int numberRolled, String playerName, boolean isHidden){
-		int result = 0;
-		for (int i = 0; i < numberRolled; i++){
-			result += (int) Math.ceil(100 * Math.random());
-		}
-		if(numberRolled == 1){
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a " + result + " on a d100!");
-		}
-		else{
-			TextMessage t = new TextMessage("Server", "Common", playerName + " Rolled a total of " + result + " out of " + numberRolled + " d100!");
-		}
-		
-		return result;
-	}
 }
