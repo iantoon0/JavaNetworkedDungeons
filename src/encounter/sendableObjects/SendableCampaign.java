b2c369fa9b049e66c53eace4893a0158a7f3c379
iantoon0 <ihunickesmith@gmail.com>
2016-11-30 11:56:18 -0600

    Continued work on fixing server-client communication

--- src/encounter/sendableObjects/SendableCampaign.java
+++ src/encounter/sendableObjects/SendableCampaign.java
@@ -1,5 +1,36 @@
 package encounter.sendableObjects;
 
-public class SendableCampaign {
+import java.util.ArrayList;
 
+import encounter.Campaign;
+import encounter.Dungeon;
+import encounter.Hero;
+import encounter.Time;
+
+public class SendableCampaign {
+	public ArrayList<SendableHero> listParty;
+	public Time currentTime;
+	public boolean bInEncounter;
+	public ArrayList<SendableDungeon> listDungeons;
+	public SendableDungeon currentTempDungeon;
+	
+	public SendableCampaign(Campaign c, boolean bPlayerIsDM, Hero h){
+		if (c.listParty != null){
+			for(Hero h2 : c.listParty){
+				listParty.add(new SendableHero(h2));
+			}
+		}
+		if (c.listDungeons != null){
+			for(Dungeon d : c.listDungeons){
+				listDungeons.add(new SendableDungeon(d, h));
+			}
+		}
+		if (c.currentDungeon != null){
+			currentTempDungeon = new SendableDungeon(c.currentDungeon, h);
+		}
+		if (c.currentTime != null){
+			this.currentTime = c.currentTime;
+		}
+		this.bInEncounter = c.bInEncounter;
+	}
 }
--- src/encounter/sendableObjects/SendableHero.java
+++ src/encounter/sendableObjects/SendableHero.java
@@ -1,5 +1,38 @@
 package encounter.sendableObjects;
 
-public class SendableHero {
+import java.util.ArrayList;
 
+import encounter.Hero;
+import encounter.Spell;
+
+public class SendableHero {
+	public int iLevel, iXP, iProficiencyBonus, iNextLvlXP, iGold,iHPGainedPerLevel, iSize;
+	public int[] iArrayCurrentSpellSlots, iArrayMaxSpellSlots;
+	public ArrayList<Integer> iArrayMaxHitDice, iArrayCurrentHitDice;
+	public boolean bInspiration;
+	public String sName, sRace, sClassName;
+	public ArrayList<String> listProficiencies, listCantripsKnown, listLanguages, listSkillProficiencies, listSpellsKnown, listSpellsPrepared, listFeats, listBackgroundTraits, listInventory;
+	
+	public SendableHero(Hero h){
+		this.iLevel = h.iLevel;
+		this.iXP = h.iXP;
+		this.iProficiencyBonus = h.iProficiencyBonus;
+		this.iNextLvlXP = h.iNextLvlXP;
+		this.iGold = h.iGold;
+		this.iHPGainedPerLevel = h.iHPGainedPerLevel;
+		this.iSize = h.iSize;
+		this.iArrayCurrentHitDice = h.iArrayCurrentHitDice;
+		this.iArrayMaxHitDice = h.iArrayMaxHitDice;
+		this.bInspiration = h.bInspiration;
+		this.sName = h.sName;
+		this.sRace = h.sRace;
+		this.sClassName = h.sClassName;
+		this.listProficiencies = h.listProficiencies;
+		this.listCantripsKnown = h.listCantripsKnown;
+		this.listLanguages = h.listLanguages;
+		this.listSkillProficiencies = h.listSkillProficiencies;
+		for(Spell s : h.listSpellsKnown){
+		//	this.listSpellsKnown.add();
+		}
+	}
 }
